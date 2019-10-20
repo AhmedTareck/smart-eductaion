94b62828bc3936d97ab79f934e809af77d10e4a7
abdullah elamir <abdullahelameer@gmail.com>
2019-10-20 16:54:02 +0200

    all

--- Managegment/Program.cs
+++ Managegment/Program.cs
@@ -14,12 +14,14 @@ namespace Management
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            BuildWebHost(args)
+                .Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+            
                 .Build();
 
     }
