7b1c52b18a907d558bbc93929cc4e9c9e14659b6
Ahmed BenSuliman <b13letu74@gmail.com>
2019-11-02 06:37:19 +0200

    inbox remove sent mail

--- Management/Controllers/MessagesController.cs
+++ Management/Controllers/MessagesController.cs
@@ -44,6 +44,7 @@ namespace Managegment.Controllers
                                       where
                                       p.IsDelete ==0 &&
                                       p.RecivedBy == userId &&
+                                      p.SentBy!=userId &&
                                       p.Status != 0 &&
                                       p.Status != 3 &&
                                       p.Status != 4 &&
