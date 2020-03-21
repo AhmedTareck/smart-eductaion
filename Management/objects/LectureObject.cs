40e4ab77d89230814d57182b23c3f7be2413192f
Mohammed Sadek <moh4sadek@gmail.com>
2020-03-21 21:27:48 +0200

    push Api mobile

---
+++ Management/objects/LectureObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Management.objects
+{
+    public class LectureObject
+    {
+
+        public long? shapterSelected { get; set; }
+        public string Name { get; set; }
+        public int? Number { get; set; }
+        public string decreption { get; set; }
+        public List<BaseFile> attashFile = new List<BaseFile>();
+        public List<BaseFile> sound = new List<BaseFile>();
+        public List<BaseFile> Video = new List<BaseFile>();
+        public List<BaseFile> Photo = new List<BaseFile>();
+
+
+    }
+
+    public class BaseFile
+    {
+        public string FileName { get; set; }
+        public string FileBase64 { get; set; }
+    }
+
+
+}
