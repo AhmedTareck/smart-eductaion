6ba2d117a803c6d29aa79dd0ae6a7a2a3019be39
abdullah elamir <abdullahelameer@gmail.com>
2020-04-02 10:22:09 +0200

    change navbar

--- Web/App/Components/Layout/AppHeader/AppHeader.js
+++ Web/App/Components/Layout/AppHeader/AppHeader.js
@@ -16,7 +16,7 @@
             circleUrl: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
             squareUrl: "https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png",
             sizeList: ["large", "medium", "small"],
-            NavBar: 'navbar navbar-main navbar-expand-lg bg-primary navbar-dark headroom',
+            NavBar: 'navbar navbar-expand-lg navbar-dark bg-default',
         };
     },
   
@@ -33,14 +33,14 @@
             } else if (route === "SignUp") {
                 this.NavBar = "navbar navbar-main navbar-expand-lg navbar-transparent headroom";
             } else {
-                this.NavBar = 'navbar navbar-main navbar-expand-lg bg-primary navbar-dark headroom';
+                this.NavBar = 'navbar navbar-expand-lg navbar-dark bg-default';
             }
         },
         href(url, visbilty) {
             if (visbilty === 1) {
                 this.NavBar = "navbar navbar-main navbar-expand-lg navbar-transparent headroom";
             } else {
-                this.NavBar = "navbar navbar-main navbar-expand-lg bg-primary navbar-dark headroom";
+                this.NavBar = "navbar navbar-expand-lg navbar-dark bg-default";
             }
             this.$router.push(url);
         }
